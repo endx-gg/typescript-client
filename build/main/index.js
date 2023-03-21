3d7ac949775ad4ed93150d393428e5d1b815793f
Mark Ramotowski <mark.tint.ramotowski@gmail.com>
2023-03-21 10:51:19 +0000

    Cleaner

---
+++ build/main/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createTradingClient = void 0;
+var client_1 = require("./lib/client");
+Object.defineProperty(exports, "createTradingClient", { enumerable: true, get: function () { return client_1.createTradingClient; } });
+// const f = async () => {
+//   const endx = await createTradingClient(
+//     'sandbox',
+//     '21f7c1dea4e2a641f13b00f759fa8de5d26900469c46e16496f773032741491d'
+//   );
+//   // endx.subscribeToOrders((d) => {
+//   //   console.log(d);
+//   // });
+//   // endx.subscribeToPortfolio((d) => {
+//   //   console.log(d);
+//   // });
+//   endx.subscribeToOrderBook('25040', (d) => {
+//     console.log(d);
+//   });
+//   console.log(await endx.instruments());
+// };
+// f();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsdUNBQW1EO0FBQTFDLDZHQUFBLG1CQUFtQixPQUFBO0FBRTVCLDBCQUEwQjtBQUMxQiw0Q0FBNEM7QUFDNUMsaUJBQWlCO0FBQ2pCLHlFQUF5RTtBQUN6RSxPQUFPO0FBQ1AsdUNBQXVDO0FBQ3ZDLHlCQUF5QjtBQUN6QixXQUFXO0FBQ1gsMENBQTBDO0FBQzFDLHlCQUF5QjtBQUN6QixXQUFXO0FBQ1gsZ0RBQWdEO0FBQ2hELHNCQUFzQjtBQUN0QixRQUFRO0FBQ1IsMkNBQTJDO0FBQzNDLEtBQUs7QUFFTCxPQUFPIn0=
